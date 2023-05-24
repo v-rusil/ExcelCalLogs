a9c3cec07528b2580ec47e2467a984e2949f144c
Rui Silva <silva.rui@microsoft.com>
2023-05-24 10:43:37 +0100

    fixing and improvements on table creation, added new json configs

---
+++ src/taskpane/buildTimeLine/CDLToEnglishActionsTimeline.ts
@@ -0,0 +1,18 @@
+//read worksheet table row by row and compare with previous row. output differences according to below rules
+//1. if the row is new, output the row (Subject: col.NormalizedSubject, Organizer: col.SentRepresentingDisplayName, Sender: col.SentRepresentingDisplayName, To List: col.DisplayAttendeesAll, Location: col.Location)
+//generate code below
+
+import { calendarItemTypes, rt, scn } from "./hashTablesCDLTimeline";
+
+export class CDLToEnglishActionsTimeline 
+{
+   
+    
+
+
+
+
+
+
+
+}
